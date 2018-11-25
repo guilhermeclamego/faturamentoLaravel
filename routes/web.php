74667b9ddd70cc21e6ca2241b0588ab1415665e6
guilhermeclamego <guilhermecesarl20@gmail.com>
2018-11-25 02:11:05 -0200

    Adicionado auth em todas as rotas

--- routes/web.php
+++ routes/web.php
@@ -6,42 +6,38 @@ Route::get('/', function () {
 
 //Rotas do Produto
 Route::get('/produtos', "ProdutoController@lista")->middleware('auth');
-Route::get('/produtos', 'ProdutoController@lista');
-Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra')->where('id', '[0-9]+');
-Route::get('/produtos/novo', 'ProdutoController@novo');
-Route::post('/produtos/adiciona', 'ProdutoController@adiciona');
-Route::get('/produtos/remove/{id}', 'ProdutoController@remove');
-Route::get('/produtos/busca/{id}', 'ProdutoController@busca')->where('id', '[0-9]+');;
-Route::post('/produtos/altera', 'ProdutoController@altera');
+Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra')->where('id', '[0-9]+')->middleware('auth');
+Route::get('/produtos/novo', 'ProdutoController@novo')->middleware('auth');
+Route::post('/produtos/adiciona', 'ProdutoController@adiciona')->middleware('auth');
+Route::get('/produtos/remove/{id}', 'ProdutoController@remove')->middleware('auth');
+Route::get('/produtos/busca/{id}', 'ProdutoController@busca')->where('id', '[0-9]+')->middleware('auth');
+Route::post('/produtos/altera', 'ProdutoController@altera')->middleware('auth');
 
 //Rotas da Produto
 Route::get('/pessoas', "PessoaController@lista")->middleware('auth');
-Route::get('/pessoas', 'PessoaController@lista');
-Route::get('/pessoas/mostra/{id}', 'PessoaController@mostra')->where('id', '[0-9]+');
-Route::get('/pessoas/novo', 'PessoaController@novo');
-Route::post('/pessoas/adiciona', 'PessoaController@adiciona');
-Route::get('/pessoas/remove/{id}', 'PessoaController@remove');
-Route::get('/pessoas/busca/{id}', 'PessoaController@busca')->where('id', '[0-9]+');;
-Route::post('/pessoas/altera', 'PessoaController@altera');
+Route::get('/pessoas/mostra/{id}', 'PessoaController@mostra')->where('id', '[0-9]+')->middleware('auth');
+Route::get('/pessoas/novo', 'PessoaController@novo')->middleware('auth');
+Route::post('/pessoas/adiciona', 'PessoaController@adiciona')->middleware('auth');
+Route::get('/pessoas/remove/{id}', 'PessoaController@remove')->middleware('auth');
+Route::get('/pessoas/busca/{id}', 'PessoaController@busca')->where('id', '[0-9]+')->middleware('auth');
+Route::post('/pessoas/altera', 'PessoaController@altera')->middleware('auth');
 
 //Rotas da Condição da Pagamento
 Route::get('/condpags', "CondpagController@lista")->middleware('auth');
-Route::get('/condpags', 'CondpagController@lista');
-Route::get('/condpags/novo', 'CondpagController@novo');
-Route::post('/condpags/adiciona', 'CondpagController@adiciona');
-Route::get('/condpags/remove/{id}', 'CondpagController@remove');
-Route::get('/condpags/busca/{id}', 'CondpagController@busca')->where('id', '[0-9]+');;
-Route::post('/condpags/altera', 'CondpagController@altera');
+Route::get('/condpags/novo', 'CondpagController@novo')->middleware('auth');
+Route::post('/condpags/adiciona', 'CondpagController@adiciona')->middleware('auth');
+Route::get('/condpags/remove/{id}', 'CondpagController@remove')->middleware('auth');
+Route::get('/condpags/busca/{id}', 'CondpagController@busca')->where('id', '[0-9]+')->middleware('auth');;
+Route::post('/condpags/altera', 'CondpagController@altera')->middleware('auth');
                         
 //Rotas do Pedido
 Route::get('/pedidos', "PedidoController@lista")->middleware('auth');
-Route::get('/pedidos', 'PedidoController@lista');
-Route::get('/pedidos/mostra/{id}', 'PedidoController@mostra')->where('id', '[0-9]+');
-Route::get('/pedidos/novo', 'PedidoController@novo');
-Route::post('/pedidos/adiciona', 'PedidoController@adiciona');
-Route::get('/pedidos/remove/{id}', 'PedidoController@remove');
-Route::get('/pedidos/busca/{id}', 'PedidoController@busca')->where('id', '[0-9]+');;
-Route::post('/pedidos/altera', 'PedidoController@altera');
+Route::get('/pedidos/mostra/{id}', 'PedidoController@mostra')->where('id', '[0-9]+')->middleware('auth');
+Route::get('/pedidos/novo', 'PedidoController@novo')->middleware('auth');
+Route::post('/pedidos/adiciona', 'PedidoController@adiciona')->middleware('auth');
+Route::get('/pedidos/remove/{id}', 'PedidoController@remove')->middleware('auth');
+Route::get('/pedidos/busca/{id}', 'PedidoController@busca')->where('id', '[0-9]+')->middleware('auth');
+Route::post('/pedidos/altera', 'PedidoController@altera')->middleware('auth');
 
 
 Auth::routes();
